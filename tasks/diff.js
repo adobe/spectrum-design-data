54af3f8556139221cdba33be4fefbf482f4f1d2d
Garth Braithwaite <garthdb@gmail.com>
2022-10-05 15:01:42 -0600

    fix: typo

--- tasks/diff.js
+++ tasks/diff.js
@@ -32,25 +32,25 @@ async function run() {
   });
   // console.log(JSON.stringify(diffResult, '', 2));
   if (Object.keys(diffResult.added).length > 0) {
-    console.log("\n*New Tokens Added:*");
+    console.log(`\n*New Tokens Added (${Object.keys(diffResult.added).length}):*`);
     Object.keys(diffResult.added).sort().forEach((tokenName, i) => {
       console.log(`  - \`${tokenName}\``);
     });
   }
   if (Object.keys(diffResult.deleted).length > 0) {
-    console.log("\n*Tokens removed:*");
+    console.log(`\n*Tokens removed (${Object.keys(diffResult.deleted).length}):*`);
     Object.keys(diffResult.deleted).sort().forEach((tokenName, i) => {
       console.log(`  - \`${tokenName}\``);
     });
   }
   if (Object.keys(diffResult.updated).length > 0) {
-    console.log("\n*Token values updated:*");
+    console.log(`\n*Token values updated (${Object.keys(diffResult.updated).length}):*`);
     Object.keys(diffResult.updated).sort().forEach((tokenName, i) => {
       console.log(`  - \`${tokenName}\``);
     });
   }
   if (Object.keys(diffResult.possiblyRenamed).length > 0) {
-    console.log("\n*Potentially rename tokens:*");
+    console.log(`\n*Potentially renamed tokens (${Object.keys(diffResult.possiblyRenamed).length}):*`);
     Object.keys(diffResult.possiblyRenamed).sort().forEach((tokenName, i) => {
       console.log(`  - \`${tokenName}\` -> \`${diffResult.possiblyRenamed[tokenName]}\``);
     });
